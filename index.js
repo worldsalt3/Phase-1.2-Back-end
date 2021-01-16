1ba750c3447907805fab724aeab3ffbc141275f4
Worldsalt3 <riliwanhazzan@gmail.com>
2021-01-16 04:34:49 +0100

    add endpoint

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+const Request = require('request')
+const url = `https://api.exchangeratesapi.io/latest`
+
+let data;
+
+Request.get(url, (error, response, body) => {
+  if (error) {
+    return console.dir(error)
+  }
+  data = {
+    results: {
+      base: JSON.parse(body).base,
+      date: JSON.parse(body).data,
+      rates: JSON.parse(body).rates,
+    },
+  }
+      
+})
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/rates', (req, res) => {
+  res.send(data)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
