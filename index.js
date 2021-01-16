3e269c00b5394f0740d585a5bb34d9e3b838d9c8
Worldsalt3 <riliwanhazzan@gmail.com>
2021-01-16 12:30:08 +0100

    add start to package.json also add port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+
 const Request = require('request')
 
 
@@ -31,6 +31,8 @@ app.get('/api/rates', (req, res) => {
   res.send(data)
 })
 
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
